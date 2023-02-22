85d192dcd18406ad210296fef70d74d4ffb5e9ff
datacurrentsea <giomola@berkeley.edu>
2023-02-21 23:34:43 -0800

    latest for lab2

--- lab_2/lab2/src/main.py
+++ lab_2/lab2/src/main.py
@@ -3,9 +3,37 @@ import uvicorn
 import os
 import json
 import re
-from pydantic import BaseModel
+import joblib
+import numpy as np
+from pydantic import BaseModel, ValidationError, validator
+from datetime import timezone, datetime
 
 app = FastAPI()
+
+class HouseData(BaseModel):
+    income: float
+    age: float
+    rooms: float
+    beds: float
+    pop: float
+    occupants: float
+    lat: float
+    long: float
+
+class HouseList(BaseModel):
+    data: list[HouseData]
+
+class HousePrice(BaseModel):
+    price: float
+
+
+model = joblib.load("model_pipeline.pkl")
+
+@validator('income')
+def income_positive(cls, v):
+    if v < 0:
+        raise ValueError('Income must be greater than 0')
+    return v
 
 @app.get("/")
 async def not_implemented():
@@ -16,9 +44,23 @@ async def hello(name: str|None = None):
     if name is not None and name != "" and name!=" ":
         return {"Hello":name}
     else:
-        raise HTTPException(status_code=400, detail="[400 Error] Name is not specified")
-@app.post("/predict")
-async def predict(test:str|None = None):
-    return {"Start": "youre not done"}
+        raise HTTPException(status_code=422, detail="[422 Error] Name is not specified")
+
+@app.get("/health")
+async def health():
+    utc_time = datetime.now(timezone.utc)
+    utc_var = datetime.isoformat(utc_time)
+    return {"UTC Time": utc_var}
+
+
+@app.post("/predict/",response_model=HousePrice)
+async def predict_price(housedata: HouseData):
+    house_dict = housedata.dict()
+    house_vals = list(house_dict.values())
+    house_vector = np.array(house_vals)
+    house_price = model.predict(house_vector.reshape(-1,8))
+    return {"price":house_price[0]}
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    #uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000,reload=True,workers=1,reload_dirs=["."])
--- lab_2/lab2/trainer/predict.py
+++ lab_2/lab2/trainer/predict.py
@@ -17,15 +17,22 @@ x = np.array(
         [8.3252, 41.0, 6.98412698, 1.02380952, 322.0, 2.55555556, 37.88, -122.23],
     ]
 )
-
+print(type(x))
 print(f"shape of input data: {x.shape}")
 start = datetime.now()
 model.predict(x)
+result = model.predict(x)
+print(result)
 end = datetime.now()
 print(f"time to perform vectorized predictions: {end-start}")
 
 start = datetime.now()
 for item in x:
+    #print("the Item:",item)
+    #print(item.shape)
+    new_shape = item.reshape(-1,8)
+    #print(new_shape)
+    #print(new_shape.shape)
     model.predict(item.reshape(-1, 8))
 end = datetime.now()
 print(f"time to perform for-loop iterated predictions: {end-start}")
